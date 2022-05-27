99b2be28004962430b0119199af86f26fa53a179
saezurucrow <usobukuhito@gmail.com>
2022-05-27 20:29:34 +0900

    usersController作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'users#index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, except: :index
 end

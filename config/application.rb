e07c07a0d1273806d1483a3f586a245c6ac54580
saezurucrow <usobukuhito@gmail.com>
2022-05-28 11:47:36 +0900

    日本語化

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Sdvxest
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
   end
 end

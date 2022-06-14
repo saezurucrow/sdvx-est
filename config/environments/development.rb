74ef03797e5e5fcca095b415510ef28caa4edc46
saezurucrow <usobukuhito@gmail.com>
2022-06-14 23:23:34 +0900

    password再設定機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -78,4 +78,15 @@ Rails.application.configure do
 
   # 本番環境時と開発環境で値が変わる
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domain: 'smtp.gmail.com',
+    user_name: 'usobukuhito@gmail.com',
+    password: 'zovkytjaidbnanfn',
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
 end

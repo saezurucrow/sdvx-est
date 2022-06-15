c3b4a91cf7c5e971ca55043a191d2654c4fbf32e
saezurucrow <usobukuhito@gmail.com>
2022-06-15 14:52:27 +0900

    maxが0の人ははランキングに表示されないように

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -11,6 +11,8 @@ class RankingsController < ApplicationController
   end
 
   def max
-    @users = User.includes(%i[ex_scores upload_statuses]).where.not(upload_statuses: { id: nil }).sort_by { |user| -user.ex_scores.s_puc_count(user.id) }
+    @users = User.includes(%i[ex_scores upload_statuses]).where.not(upload_statuses: { id: nil }).select do |x|
+               x.ex_scores.s_puc_count(x.id) > 0
+             end.sort_by { |user| -user.ex_scores.s_puc_count(user.id) }
   end
 end

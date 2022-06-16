09f1cab0658e5b45dcd21d9b6f6bda0f1be8214d
saezurucrow <usobukuhito@gmail.com>
2022-06-17 06:06:14 +0900

    メーラーの指定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,4 +120,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domain: 'smtp.gmail.com',
+    user_name: 'usobukuhito@gmail.com',
+    password: 'zovkytjaidbnanfn',
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
 end

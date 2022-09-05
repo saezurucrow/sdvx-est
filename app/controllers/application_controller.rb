f1dd0ab9b5243390fdbe662832060fd493f14b20
saezurucrow <usobukuhito@gmail.com>
2022-09-06 00:47:40 +0900

    update 20220906

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception

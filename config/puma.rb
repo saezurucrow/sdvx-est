fc848a3290b4d9b2bcff18f7c11f5a4bce1fd24a
saezurucrow <usobukuhito@gmail.com>
2023-03-05 06:03:35 +0900

    asdf;

--- config/puma.rb
+++ config/puma.rb
@@ -18,7 +18,7 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 # port ENV.fetch('PORT', 3000)
-bind 'unix:///var/www/$APP_NAME/shared/tmp/sockets/puma.sock'
+bind 'unix:///var/www/html/sdvx_est/shared/tmp/sockets/puma.sock'
 
 # Specifies the `environment` that Puma will run in.
 #

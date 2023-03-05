51c23d63bea0ecf048adc3e4fda6901b304d3a5a
saezurucrow <usobukuhito@gmail.com>
2023-03-05 16:57:20 +0900

    asdf

--- app/models/favorite_song.rb
+++ app/models/favorite_song.rb
@@ -9,4 +9,8 @@ class FavoriteSong < ApplicationRecord
   def self.ransackable_attributes(_auth_object = nil)
     %w[created_at id song_id updated_at user_id]
   end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[song user]
+  end
 end

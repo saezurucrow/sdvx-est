ed24020a5d58e17833656d546093e95b67801dc6
saezurucrow <40511624+saezurucrow@users.noreply.github.com>
2023-04-13 23:53:12 +0900

    add admin upload_status delete button (#40)

--- app/controllers/admins/upload_statuses_controller.rb
+++ app/controllers/admins/upload_statuses_controller.rb
@@ -17,6 +17,32 @@ module Admins
       @upload_status = UploadStatus.includes(ex_score_differences: [{ ex_score: :song }]).find(params[:id])
     end
 
+    def destroy
+      # UploadStatus.find(id).ex_score_differences分繰り返す
+      upload_status_id = params[:id]
+
+      UploadStatus.find(upload_status_id).ex_score_differences.each do |ex_score_difference|
+        ex_score_id = ex_score_difference.ex_score_id
+
+        # ex_score_differenceの削除
+        ex_score_difference.delete
+
+        # ex_scoreの削除
+        ExScore.find(ex_score_id).delete
+      rescue ActiveRecord::RecordNotFound => e
+        next
+      end
+
+      # UploadStatusの削除
+      if UploadStatus.find(upload_status_id).delete
+        flash.now[:notice] = '削除しました'
+      else
+        flash.now[:alert] = '削除できませんでした'
+      end
+
+      redirect_to admins_upload_statuses_path
+    end
+
     private
 
     def is_admin!
